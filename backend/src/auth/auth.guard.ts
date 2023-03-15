09c5f7e8ec4b92f1693ea70c80a0669f32c0a6e8
web3ten0 <web3ten0@gmail.com>
2023-03-16 00:11:23 +0900

    Github Copilotのリファクタリング

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -5,13 +5,31 @@ import { decode } from 'next-auth/jwt';
 @Injectable()
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Get the context passed to the resolver
     const ctx = GqlExecutionContext.create(context).getContext();
+
+    // Extract the authorization header from the request
     const authorization = ctx.req.headers?.authorization;
-    const token = authorization?.split(' ')[1];
+    if (!authorization) return false;
+
+    // Extract the token from the authorization header
+    const token = authorization.split(' ')[1];
+    if (!token) return false;
+
+    // Extract the secret from the environment
     const secret = process.env.NEXTAUTH_SECRET ?? '';
-    const decoded = await decode({ token, secret });
-    if (!decoded) return false;
-    ctx.user = decoded;
-    return true;
+    if (!secret) return false;
+
+    // Decode the JWT token using the secret
+    try {
+      const decoded = await decode({ token, secret });
+      if (!decoded) return false;
+      // Add the decoded token to the context
+      ctx.user = decoded;
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
   }
 }
